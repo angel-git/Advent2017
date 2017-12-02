19d8c964ca2e756b3b399594bf57aab2b0e12517
Angel Gavalda <agavalda@xebialabs.com>
2017-12-02 12:38:59 +0100

    day 2 cleaning

--- src/main/kotlin/day2/CorruptionChecksum.kt
+++ src/main/kotlin/day2/CorruptionChecksum.kt
@@ -1,23 +1,25 @@
 package day2
 
-fun checksum(input: String): Int {
+private fun doChecksum(input: String, action: (List<Int>) -> Int): Int {
     var sum = 0
     input.reader().forEachLine {
         val numbers = it.split("\t").map { it.toInt() }
-        sum += numbers.max()!! - numbers.min()!!
+        sum += action(numbers)
 
     }
     return sum
 }
 
-fun checksum2(input: String): Int {
-    var sum = 0
-    input.reader().forEachLine {
-        val numbers = it.split("\t").map { it.toInt() }
-        numbers.forEachIndexed { indexI, i ->
-            numbers.forEachIndexed { indexJ, j -> if (indexI != indexJ && i % j == 0) sum += i / j }
-        }
+fun checksum(input: String): Int {
+    return doChecksum(input, { it.max()!! - it.min()!! })
+}
 
-    }
-    return sum
+fun checksum2(input: String): Int {
+    return doChecksum(input, {
+        var sum = 0
+        it.forEachIndexed { indexI, i ->
+            it.forEachIndexed { indexJ, j -> if (indexI != indexJ && i % j == 0) sum += i / j }
+        }
+        sum
+    })
 }
